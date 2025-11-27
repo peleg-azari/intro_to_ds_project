9b2f9dda9c17b17c0389be1ddb876553c79e646e
Peleg Az-Ari <pelegazari@gmail.com>
2025-11-27 13:24:25 +0200

    branch test

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'{name} created branch')
+    print(f'this branch is deleted')
 
 
 # Press the green button in the gutter to run the script.
